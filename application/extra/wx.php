d2d3d8637c29d7b34b8320f8a55a6c287a53e9ba
bujxu <30498959+bujxu@users.noreply.github.com>
2018-12-10 21:49:01 +0800

    update app secret and app id

--- application/extra/wx.php
+++ application/extra/wx.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'app_id' => 'wxb27bd846bfec0588',
-    'app_secret' => '8a5e8ed00ac202e77db7778e9f388f8b',
+    'app_id' => 'wxa4454c476ce4db86',
+    'app_secret' => 'b4784ae8caec2abec89b966ccb3b8dd2',
     'login_url' => 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
 ];
