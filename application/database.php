e977fc3b436cc1e5c02be1f8691e797813753b10
bujxu <30498959+bujxu@users.noreply.github.com>
2018-12-10 20:44:30 +0800

    database

--- application/database.php
+++ application/database.php
@@ -15,9 +15,9 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'sharefavorite',
+    'database'        => 'express',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'admin',
     // 密码
     'password'        => '123456',
     // 端口
